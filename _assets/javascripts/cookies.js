072e8a24186b27060a56ab768b6ae16391faf55d
Tadej Novak <tadej@tano.si>
2015-09-20 17:42:32 +0200

    Run through code beautifier

--- _assets/javascripts/cookies.js
+++ _assets/javascripts/cookies.js
@@ -1,8 +1,6 @@
-var cookies =
-{
+var cookies = {
     // Function to call on load
-    showMessageLoad: function()
-    {
+    showMessageLoad: function() {
         if ($.cookie('cookies_enable')) {
             $('#cookie_message').css('display:', 'none');
         } else {
@@ -11,27 +9,32 @@ var cookies =
     },
 
     // To hide the message but show it again on page reload
-    hideMessage: function()
-    {
-        $('#cookie_message').fadeOut(200, function() { $(this).remove(); });
+    hideMessage: function() {
+        $('#cookie_message').fadeOut(200, function() {
+            $(this).remove();
+        });
     },
 
     // To hide the hide the message and set the cookie so when the page is reloaded the message does not appear
-    hideForever: function()
-    {
+    hideForever: function() {
         cookies.hideMessage();
-        $.cookie('cookies_enable', true, { expires: 180, path: '/', domain: 'tano.si' });
+        $.cookie('cookies_enable', true, {
+            expires: 180,
+            path: '/',
+            domain: 'tano.si'
+        });
     },
 
     // Optional function to show message manually on the demo page
-    showMessage: function()
-    {
+    showMessage: function() {
         $('#cookie_message').show();
     },
 
     // Delete cookie
-    delete: function()
-    {
-        $.removeCookie('cookies_enable', { path: '/', domain: 'tano.si' });
+    delete: function() {
+        $.removeCookie('cookies_enable', {
+            path: '/',
+            domain: 'tano.si'
+        });
     }
 };
--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,7 +1,6 @@
 //= require cookies
 
-$(document).ready(function()
-{
+$(document).ready(function() {
     // Variables
     var body = $('body'),
         win = $(window),
@@ -16,9 +15,8 @@ $(document).ready(function()
         }
     };
 
-    function init()
-    {
-        $("a[href^='#']").smoothScroll();    
+    function init() {
+        $("a[href^='#']").smoothScroll();
         cookies.showMessageLoad();
 
         $('[data-toggle=offcanvas]').click(function() {
